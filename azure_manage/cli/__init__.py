9a4c468680ce08f5950fb900ea0ac72790e8b8df
Bastian Blank <bastian.blank@credativ.de>
2015-10-26 09:21:04 +0100

    Add option parameter to command line

--- azure_manage/cli/__init__.py
+++ azure_manage/cli/__init__.py
@@ -7,17 +7,32 @@ import argparse
 from ..config import Config
 
 
+class ArgsActionDict(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string):
+        namespace_values = getattr(namespace, self.dest, None) or {}
+
+        key, value = values.split('=', 1)
+        namespace_values[key] = value
+
+        setattr(namespace, self.dest, namespace_values)
+
+
 def setup_argparse(description):
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--auto', action='store_true')
     parser.add_argument('--config', metavar='CONFIG', default=None)
-    parser.add_argument('--workdir', metavar='WORKDIR', help='working directory (default: ./SECTION-VERSION)')
+    parser.add_argument('--option', metavar='OPTION=VALUE', dest='options',
+            default={}, action=ArgsActionDict)
+    parser.add_argument('--workdir', metavar='WORKDIR',
+            help='working directory (default: ./SECTION-VERSION)')
     parser.add_argument('section', metavar='SECTION')
     parser.add_argument('version', metavar='VERSION')
     return parser
 
 
 class CliBase:
+    parser = setup_argparse(description='Base')
+
     def __init__(self):
         self.args = self.parser.parse_args()
 
---
+++ azure_manage/cli/test___init__.py
@@ -0,0 +1,11 @@
+from . import CliBase
+
+
+def test_CliBase_parser():
+    parser = CliBase.parser
+    options = parser.parse_args(['section', 'version'])
+    assert options.section == 'section'
+
+    options = parser.parse_args(['--option', 'key1=value1', '--option', 'key2=value2', 'section', 'version'])
+    assert options.options['key1'] == 'value1'
+    assert options.options['key2'] == 'value2'
