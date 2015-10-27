d2aecbde1910d53814493dca3b3df2fdd49a37b5
Bastian Blank <bastian.blank@credativ.de>
2015-10-27 13:40:17 +0000

    Add minimal list_images tool

---
+++ azure_manage/cli/list_images.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright: 2015 Bastian Blank
+# License: MIT, see LICENSE.txt for details.
+
+import argparse
+import os
+from pprint import pprint
+
+from ..servicemanagementservice import ServiceManagementService
+from . import CliBase, setup_argparse
+
+
+class Cli(CliBase):
+    parser = setup_argparse(description='List images')
+    parser.add_argument('--category', metavar='CATEGORY', default=None,
+            help='filter on category')
+
+    def __init__(self):
+        super().__init__()
+
+        self.image_family = self.config_get('image_meta')['image_family']
+
+        self.subscription = self.config_get('subscription')
+        self.subscription_keyfile = self.config_get('subscription_keyfile')
+
+    def __call__(self):
+        servicemanager = ServiceManagementService(self.subscription, self.subscription_keyfile)
+        for i in servicemanager.list_os_images():
+            if i.image_family != self.image_family:
+                continue
+            if self.args.category and i.category != self.args.category:
+                continue
+
+            print(i.name)
+
+
+def main():
+    Cli()()
+
+
+if __name__ == '__main__':
+    main()
